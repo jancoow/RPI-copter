432327528e1265a57e17992a5847d1e259985f22
Janco Kock <jancokock@gmail.com>
2015-12-22 17:15:47 +0100

    Code styling

--- quadcopter-pi/PID.py
+++ quadcopter-pi/PID.py
@@ -13,44 +13,16 @@ class PID:
         self.d_gain = float(dgain)
 
     def Compute(self, input, target, dt):
-        #-------------------------------------------------------------------------------------------
-        # Error is what the PID alogithm acts upon to derive the output
-        #-------------------------------------------------------------------------------------------
         error = target - input
 
-        #-------------------------------------------------------------------------------------------
-        # The proportional term takes the distance between current input and target
-        # and uses this proportially (based on Kp) to control the ESC pulse width
-        #-------------------------------------------------------------------------------------------
         p_error = error
-
-        #-------------------------------------------------------------------------------------------
-        # The integral term sums the errors across many compute calls to allow for
-        # external factors like wind speed and friction
-        #-------------------------------------------------------------------------------------------
         self.i_error += (error + self.last_error) * dt
         i_error = self.i_error
-
-        #-------------------------------------------------------------------------------------------
-        # The differential term accounts for the fact that as error approaches 0,
-        # the output needs to be reduced proportionally to ensure factors such as
-        # momentum do not cause overshoot.
-        #-------------------------------------------------------------------------------------------
         d_error = (error - self.last_error) / dt
 
-        #-------------------------------------------------------------------------------------------
-        # The overall output is the sum of the (P)roportional, (I)ntegral and (D)iffertial terms
-        #-------------------------------------------------------------------------------------------
         p_output = self.p_gain * p_error
         i_output = self.i_gain * i_error
         d_output = self.d_gain * d_error
 
-        #-------------------------------------------------------------------------------------------
-        # Store off last error for integral and differential processing next time.
-        #-------------------------------------------------------------------------------------------
         self.last_error = error
-
-        #-------------------------------------------------------------------------------------------
-        # Return the output, which has been tuned to be the increment / decrement in ESC PWM
-        #-------------------------------------------------------------------------------------------
         return p_output + i_output + d_output
